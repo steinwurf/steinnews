bb8eb8c9892773d6ff14904a1ddad72dcfe6bf77
rolesen <rune.olesen@gmail.com>
2021-02-21 19:56:15 +0100

    Less hardcoding in tests and using datarecorder for comparison/recording

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,11 @@
 
 import glob
 
+from os.path import dirname, abspath, basename
+
 def pytest_generate_tests(metafunc):
-    filelist = glob.glob('test/data/*.rst')
-    metafunc.parametrize("file_in", filelist )
+    testdir = dirname(abspath(__file__))
+    datafilelist = glob.glob(testdir + '/data/*.rst')
+    arguments = [(datafile, testdir + '/recording/' + metafunc.function.__name__ + '/' + basename(datafile) + '.txt') for datafile in datafilelist]
+    metafunc.parametrize("file_in, recording_file", arguments)
 
--- test/test_increment_version.py
+++ test/test_increment_version.py
@@ -1,15 +1,21 @@
 from src.steinnews import write_next_version
 
-import filecmp
+import pytest_datarecorder
 import os
 
-def test_increment_version(file_in):
-
-    basename = os.path.basename(file_in)
-
-    file_out = 'test/recordings/' + basename
-    file_ref = 'test/recordings/' + basename + '.ref'
+def test_increment_version(file_in, recording_file, datarecorder, tmpdir):
+    file_out = tmpdir.join('output.rst')
 
     write_next_version(file_in, file_out)
-    assert filecmp.cmp(file_ref, file_out)
+
+    with open(file_out, 'r') as file:
+        output = file.read()
+
+
+    #suggestion: that the fixture library should attempt to create any 'intermediate' directories (it already handles creating the file part)
+    recording_dir = os.path.dirname(recording_file)
+    if not os.path.exists(recording_dir):
+        os.makedirs(recording_dir)
+    datarecorder.record_data(data=output, recording_file=recording_file)
+    
 
