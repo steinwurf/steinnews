9505c8dad923f9f01f82d985110ccce541e1790a
Bartosz Lachowicz <jendrekpb@gmail.com>
2022-09-30 00:23:45 +0300

    Added another set of parametrized tests

--- conftest.py
+++ conftest.py
@@ -4,17 +4,25 @@ import os
 
 def pytest_generate_tests(metafunc):
     testdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test")
-    datafilelist = glob.glob(os.path.join(testdir, "data") + "/*.rst")
-    arguments = [
-        (
-            datafile,
-            os.path.join(
-                testdir,
-                "recording",
-                metafunc.function.__name__,
-                os.path.basename(datafile) + ".txt",
-            ),
+
+    if metafunc.function.__name__ == "test_increment_version":
+        datafilelist = glob.glob(os.path.join(testdir, "data") + "/*.rst")
+        arguments = [
+            (
+                datafile,
+                os.path.join(
+                    testdir,
+                    "recording",
+                    metafunc.function.__name__,
+                    os.path.basename(datafile) + ".txt",
+                ),
+            )
+            for datafile in datafilelist
+        ]
+        metafunc.parametrize("file_in, recording_file", arguments)
+
+    elif metafunc.function.__name__ == "test_no_changes":
+        no_changes_datafilelist = glob.glob(
+            os.path.join(testdir, "data", "no_changes") + "/*.rst"
         )
-        for datafile in datafilelist
-    ]
-    metafunc.parametrize("file_in, recording_file", arguments)
+        metafunc.parametrize("no_changes_file_in", no_changes_datafilelist)
